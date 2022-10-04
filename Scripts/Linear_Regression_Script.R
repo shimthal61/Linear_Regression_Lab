a59522f15ac2686c77f4635da2ffff07c4990332
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2022-10-04 17:12:53 +0100

    started as markdown doc

--- Scripts/Linear_Regression_Script.R
+++ Scripts/Linear_Regression_Script.R
@@ -1,55 +1,33 @@
-library(MASS) #Hasa very large collection of dasta sets and funcitons
-library(ISLR2) # Includes the datasets assocaited with this module
+---
+output: reprex::reprex_document
+knit: reprex::reprex_render
+---
 
+Load in our packages
+
+library(MASS) # Contains our data
+library(ISLR2) # Contains the Boston data set 
+
+
+
+# Let's glimpse our dataset
 head(Boston)
 
-lm.fit <- lm(medv ~ lstat, data = Boston)
+# Output variable:
+# medv - medium house value. Our predictor variable
+
+# Input variable
+# rmvar - average number of rooms per house
+# age average age of house 
+# lstat - percent of households with low socioeconomic status
+
+# Let's use the attach command to lock on our data set
 attach(Boston)
 
-summary(lm.fit)
+# We can use the lm function to fit a simple linear regression model
 
-names(lm.fit)
+lm.fit <- lm(medv ~ lstat)
 
-coef(lm.fit) # Looking at the coefficients
+# We can view some of the characteristics of the model
+lm.fit
 
-confint(lm.fit) #97.5% CI
-
-predict(lm.fit, data.frame(lstat = (c(5, 10, 15))),
-        interval = "confidence") # Producing CI and predicted intervals of medv
-# a given value of lstat
-
-predict(lm.fit, data.frame(lstat = (c(5, 10, 15))),
-        interval = "prediction")
-
-# Plottimedv and lstat along with the least squares regression line
-plot(lstat, medv)
-abline(lm.fit)
-
-# Some evidence for non-linearity in their relationship
-
-# The lwd argument changes the width of the regression line.
-
-# Different graph types:
-plot(lstat, medv)
-abline(lm.fit, lwd = 3)
-abline(lm.fit, lwd = 3, col = "red")
-plot(lstat, medv, col = "red")
-plot(lstat, medv, pch = 20) # pch argument is for different plot point types
-plot(lstat, medv, pch = "+")
-plot(1:20, 1:20, pch = 1:20) # Showing different pch types
-
-# We can create some diagnostic plots and tell R to show all 4 plots in a 2x2 grid
-
-par(mfrow = c(2, 2))
-plot(lm.fit)
-
-# We can also compute the residuals using the residuals() function
-
-plot(predict(lm.fit), residuals(lm.fit))
-plot(predict(lm.fit), rstudent(lm.fit))
-
-plot(hatvalues(lm.fit)) # Calculating leverage statistics 
-
-# Identifies the index of the largest element of a vector. Tell us which observation
-# has the largest leverage statistic
-which.max(hatvalues(lm.fit))
